0546ca062772e39d3facd4db8d29352860bfb620
peterbaks <peterbaks081@gmail.com>
2023-06-16 00:57:51 +0300

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
+
+/*
+ *main prints "Programming is like building a multilingual puzzle
+ */
+
+
 int main(void)
+
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
